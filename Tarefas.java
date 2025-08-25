323d81d6380d154fb7c6b797fa2f1e80b08aef39
Mateus Barbosa Gromowski <mateusbarbosagromowski@gmail.com>
2025-08-25 17:24:09 -0300

    Adicionadas as funções de marcar como completa e incompleta

--- Tarefas.java
+++ Tarefas.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Tarefas {
+class Tarefas {
 	public ArrayList<String> listaDeTarefas = new ArrayList<>();
+	public ArrayList<Boolean> isCompleta = new ArrayList<>();
 	public int contagemDeTarefas = 0;
-	public String setTask;
+	public int compOuIncomp;
 
 	void apagaTarefa(Scanner scanner) {
 		System.out.print("Deseja apagar qual tarefa?: ");
@@ -13,7 +14,8 @@ public class Tarefas {
 		if (paraApagar > listaDeTarefas.size() || paraApagar < 1) {
 			System.out.println("Entrada inválida.");
 		} else {
-			listaDeTarefas.remove((paraApagar-1));
+			listaDeTarefas.remove(paraApagar - 1);
+			isCompleta.remove(paraApagar - 1);
 			System.out.println("Tarefa apagada com sucesso.");
 			contagemDeTarefas--;
 		}
@@ -24,15 +26,56 @@ public class Tarefas {
 			System.out.println("Ainda não há nenhuma tarefa. Que tal adicionar alguma?");
 		} else {
 			for (int i = 0; i < contagemDeTarefas; i++) {
-				System.out.printf("%d - %s\n",(i + 1),listaDeTarefas.get(i));
+				if (isCompleta.get(i)) {
+					System.out.printf("%d - %s - Completa\n",(i + 1),listaDeTarefas.get(i));
+				} else {
+					System.out.printf("%d - %s - Incompleta\n",(i + 1),listaDeTarefas.get(i));
+				}
 			}
 		}
 	}
 
 	void addTarefas(Scanner scanner) {
 			System.out.printf("Insira aqui a tarefa %d: ", (contagemDeTarefas + 1));
-			setTask = scanner.nextLine();
+			String setTask = scanner.nextLine();
 			listaDeTarefas.add(setTask);
+			isCompleta.add(false);
 			contagemDeTarefas++;
+	}
+
+	void completaTarefa(Scanner scanner) {
+		if (listaDeTarefas.size() == 0) {
+			System.out.println("Ainda não há nenhuma tarefa. Que tal adicionar alguma?");		
+		} else {
+			System.out.print("Qual tarefa você deseja marcar como completa?: ");
+			compOuIncomp = scanner.nextInt();
+			if (compOuIncomp < 1 || listaDeTarefas.size() < (compOuIncomp)) {
+				System.out.println("Entrada inválida.");
+			} else if (isCompleta.get(compOuIncomp - 1) == true) {
+					System.out.println("Essa tarefa já está completa.");
+				} else {
+					isCompleta.set((compOuIncomp - 1), true);
+					System.out.println("Tarefa marcada com sucesso.");
+				}
+			}
+		}
+	}
+
+	void marcaIncompleta(Scanner scanner) {
+		if (listaDeTarefas.size() == 0) {
+			System.out.println("Ainda não há nenhuma tarefa. Que tal adicionar alguma?");		
+		} else {
+			System.out.print("Qual tarefa você deseja marcar como incompleta?: ");
+			compOuIncomp = scanner.nextInt();
+			if (compOuIncomp < 1 || listaDeTarefas.size() < (compOuIncomp)) {
+				System.out.println("Entrada inválida.");
+			} else if (isCompleta.get(compOuIncomp - 1) == false) {
+					System.out.println("Essa tarefa já está incompleta.");
+				} else {
+					isCompleta.set((compOuIncomp - 1), false);
+					System.out.println("Tarefa desmarcada com sucesso.");
+				}
+			}
+		}		
 	}
 }
